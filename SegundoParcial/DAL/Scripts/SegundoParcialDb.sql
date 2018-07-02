5bd6797ea1f8cc5667da919763f5ab1243e1a59b
Samuel Alexander Reyes Diaz <samuel_reyes1@ucne.edu.do>
2018-07-02 18:04:01 -0400

    Empezo el examen

--- SegundoParcial/DAL/Scripts/SegundoParcialDb.sql
+++ SegundoParcial/DAL/Scripts/SegundoParcialDb.sql
@@ -2,3 +2,9 @@ Create database SegundoParcialDb
 go
 use SegundoParcialDb
 go
+CREATE TABLE Articulos
+(
+	VehiculoID int primary KEY identity(1,1),
+	Descripcion varchar(max),
+	Mantenimiento int,
+);
